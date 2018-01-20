bc6889eb74f4420d47daf1afb7c5e51afa82edbd
Betsy Sallee <ensallee@gmail.com>
2018-01-20 19:34:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var moreKittens= [...kittens, "Broom"];
+  return moreKittens;
+}
+
+function prependKitten(name){
+  var evenMoreKittens=["Arnold", ...kittens];
+  return evenMoreKittens;
+}
+
+function removeLastKitten(){
+  var fewerKittens=[kittens.slice()]
+}
